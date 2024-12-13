fe5d77a4dd576c9eb4565636c312e1dad73f134d
Nsbbgamers42 <benjaminspaquette@gmail.com>
2024-12-13 16:25:20 -0500

    The finale

---
+++ Labs/raidinfo.js
@@ -0,0 +1,24 @@
+function changetext(){
+  document.getElementById("welcome").innerHTML = "ORIGIN DIALGA RAID INFORMATION </p> <p> Origin Dialga is a steel/dragon type pokemon, making it weak to ground and fighting types. With a hundo cp of 2337 and 2921 Weatherboosted. Origin dialga is in raids from November 18th-27th. Being a top dragon attacker with roar of time, here are your best counters. </p> <p> Megas: Mega lucario, Primal groudon, Mega garchomp, Mega heracross, Mega blaziken and Mega alakazam. </p> <p> Shadows: Shadow groudon, Shadow excadrill, Shadow excadrill, Shadow mamoswine and Shadow garchomp. </p> <p> Regular counters: Lucario, Terrakion, Keldeo, Therian landorus, Groudon and Excadrill. </p> <br /> <br /> <p> <img src='origindialga.jpg' alt'dialhgaboi' height='200px'>  ";
+}
+
+
+
+
+
+
+function changetextagain(){
+  document.getElementById("welcome").innerHTML = "ORIGIN PALKIA RAID INFORMATION </p> <p> Origin Palkia is a water/dragon type pokemon, making it weak to dragon and fairy types. With a hundo cp of 2367 and 2958 Weatherboosted. Origin palkia is in raids from November 18th-27th. Being a top dragon attacker with spacial rend, here are your best counters. </p> <p> Megas: Mega rayquaza, Mega garchomp, Mega gardevoir, Mega salamence, Mega latios </p> <p> Shadows: Shadow salamence, Shadow dragonite, Shadow latios, Shadow garchomp, Shadow alolan exeggutor </p> <p> Regular counters: Origin palkia, Origin dialga, Haxorus, Dragapult, Dialga </p> <br /> <br /> <p> <img src='originpalkia.jpg' alt'palkiaboi' height='200px'>   ";
+}
+
+
+
+
+function change(){
+  document.getElementById("welcome").innerHTML = "PRIMAL GROUDON RAID INFORMATION </p> <p> Primal Groudon is a ground/fire type pokemon, making it weak to water(x2) and ground types. With a hundo cp of 2351 and 2939 Weatherboosted. Primal Groudon is in raids from November 23rd-24th. Being a top ground attacker with precipice blades, here are your best counters. </p> <p> Megas: Primal kyogre, Mega swampert, Mega blastoise, Mega gyarados, Mega slowbro </p> <p> Shadows: Shadow kyogre, Shadow gyarados, Shadow swampert, Shadow feraligatr, Shadow crawdaunt </p> <p> Regular counters: Kyogre, Golisopod, Primarina, Swampert, Gyarados. </p> <br /> <br /> <p> <img src='groudon.jpg' alt'groudonaboi' height='200px'>   ";
+}
+
+
+function changemore(){
+  document.getElementById("welcome").innerHTML = "PRIMAL KYOGRE RAID INFORMATION </p> <p> Primal Kyogre is a water type pokemon, making it weak to Grass and electric types. With a hundo cp of 2351 and 2939 Weatherboosted. Primal Kyogre is in raids from November 23rd-24th. Being a top water attacker with origin pulse, here are your best counters. </p> <p> Megas: Mega sceptile, Mega venusaur, Mega manectric, Mega rayquaza, Mega ampharos </p> <p> Shadows: Shadow raikou, Shadow magnezone, Shadow sceptile, Shadow electivire, Shadow venusaur. </p> <p> Regular counters: Zarude, Kartana, Xurkitree, Zekrom, Raikou. </p> <br /> <br /> <p> <img src='kyogre.jpg' alt'groudonaboi' height='200px'>   ";
+}
---
+++ Labs/shinies.js
@@ -0,0 +1,49 @@
+function changetext(){
+  document.getElementById("shinies").innerHTML = "This is the base shiny rate. About 95% of shiny eligable pokemon are at this rate. </p> <br /> <br /> <p> It would take longer to list the pokemon with this rate then to list the ones without. But as a rule of thumb, if its in the wild its most likely going to be a 1/500 chance for a shiny outside events that boost the rates. </p> <br /> <p> some examples include Pikachu, Pidgey, Relicanth, Grimer, Spearow and more.";
+}
+
+
+
+
+
+
+function changetextagain(){
+  document.getElementById("shinies").innerHTML = "This shiny rate is very rare. Gible and shinx are the only known pokemon to have this rate outside events. However, for some special events such as sinnoh tour, most wild spawns during the event times are 1/256. With exceptions of course.";
+}
+
+
+
+
+function change(){
+  document.getElementById("shinies").innerHTML = "This rate is not as rare but still not the most common. Certain events will have shiny boosts for certain pokemon and this is the usual rate. For example, we had a halloween event recently where zorua was 1/128 during the event.";
+}
+
+
+function changemore(){
+  document.getElementById("shinies").innerHTML = "1/64 is a decently common rate. To the point that pokemon at this rate are called 'permaboosted pokemon'. pokemon with this rate tend to stay at this rate even outside events. There are a few ways to know if somethings permaboosted. If a shiny is completely locked in raids or eggs and is not a legendary, its most definetly permaboosted and likely will be if it ever starts spawning in the wild. Some examples of this include scyther, onix, absol, mawile, vullaby, falinks, chansey, most galarian forms and more!";
+}
+
+function changer(){
+  document.getElementById("shinies").innerHTML = "This shiny rate is reserved for community days. Community days are events that happen once a month for 3 hours where 1 pokemon will spawn everywhere with this rate. However, after the com day ends it will go back to its previous shiny rate, usually being 1/500.";
+}
+
+
+
+
+
+
+function changing(){
+  document.getElementById("shinies").innerHTML = "This rate is reserved for legendary and gigantamx pokemon. regardless how you encounter a legendary whether it be wild, raids, GBL etc, it will always have this rate unless shiny locked. ";
+}
+
+
+
+
+function choose(){
+  document.getElementById("shinies").innerHTML = "This shiny rate is reserved for research days, raid days and hatch days. These events are when a certain pokemon will be available for 3 hours within one of the methods mentioned with this shiny rate. Though they will go back to their previously established shiny rate afterwards. ";
+}
+
+
+function finalchange(){
+  document.getElementById("shinies").innerHTML = "Not every pokemon can be shiny however. Usually when a pokemon is released, they will wait about a year to release its shiny form. Though some pokemon have waited a long time for theirs. Shiny deerling took almost 5 years to release after deerling was released. Also, if a pokemon is evolved in the wild, it is shiny locked unless it has a mega evolution in the game. In which they will be either 1/64 or 1/128. ";
+}
